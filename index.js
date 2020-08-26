fbd6d9c20fdd4f2bb921cd6c6633a59837baef1e
Paul Kim <khyunwoo1@gmail.com>
2020-08-26 20:37:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,11 +34,8 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction(funkyFunction())
-
-// so the question is, how do navigate around functions within functions?
-// how do return a function within a function?
-
+var theFunk = funkyFunction();
+theFunk();
 
 
 
